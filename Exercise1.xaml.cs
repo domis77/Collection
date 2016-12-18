5e8014cdaabbe279b058e63d11eb6ac65c5a0c21
domis77 <domis77@DOMIS77-MSI>
2016-12-19 00:21:38 +0100

    exercise1

--- Exercise1.xaml.cs
+++ Exercise1.xaml.cs
@@ -21,6 +21,9 @@ namespace Collection
     /// </summary>
     public partial class Exercise1 : Window
     {
+        Counter counter;
+        //= new Counter();
+
         public Exercise1()
         {
             InitializeComponent();
@@ -38,19 +41,17 @@ namespace Collection
 
             openFileDialog.InitialDirectory = "c:\\"; //tmp
 
-
             openFileDialog.ShowDialog();
             try
             {
+                counter = new Counter();
                 if ((fileStream = openFileDialog.OpenFile()) != null)
                 {
                     using (fileStream)
                     {
                         StreamReader streamReader = new StreamReader(fileStream);
-                        while (streamReader.Peek() >= 0)
-                        {
-                            textList.Add((char)streamReader.Read());
-                        };
+
+                        counter.fillCharList(streamReader);
 
                         streamReader.BaseStream.Seek(0, SeekOrigin.Begin);
                         ContentFile_textBlock.Text = streamReader.ReadToEnd();
@@ -61,6 +62,62 @@ namespace Collection
             {
                 System.Windows.MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
             }
+        }
+
+
+        private void Check_button_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(ContentFile_textBlock.Text))
+            {
+                System.Windows.Forms.MessageBox.Show("No file load, or empty file!", null, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (string.IsNullOrEmpty(CharInput_textBox.Text))
+            {
+                System.Windows.Forms.MessageBox.Show("Not specified character!");
+            }
+            else
+            {
+                counter.countChars(CharInput_textBox.Text[0]);
+            }     
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+
+
+
+    public class Counter
+    {
+        private List<char> charList;
+        
+        public Counter()
+        {
+            charList = new List<char>();
+        }
+        
+        public void fillCharList(StreamReader stream)
+        {
+            while (stream.Peek() >= 0)
+            {
+                charList.Add((char)stream.Read());
+            }
+        }
+
+        public void countChars(char wantedChar)
+        {
+            int counter = 0;
+            foreach(var ch in charList)
+            {
+                if(ch == wantedChar)
+                {
+                    counter++;
+                }
+            }
+            System.Windows.Forms.MessageBox.Show("Character " + wantedChar + " occurred " + counter + " times.");
         }
     }
 }
